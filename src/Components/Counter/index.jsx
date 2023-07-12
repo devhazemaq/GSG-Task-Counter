d7bdfa0c3111dd72f43bc006e1284c266e9c5620
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-12 12:53:18 +0300

    Done

--- src/Components/Counter/index.jsx
+++ src/Components/Counter/index.jsx
@@ -5,14 +5,15 @@ import "./index.css"
 export default class Counter extends Component {
   render() {
 
-
+      const {id, value, handleIncrease, handleDecrease} = this.props
     return (
+      
       <div className='main__counter'> 
 
         <div className='counter__ele'>
-          <span className='span__show'>0</span>
-          <button className='btn__dec' >-</button>
-          <button className='btn__inc'>+</button>
+          <span className='span__show'>{value}</span>
+          <button className='btn__dec' disabled={!value} onClick={()=> handleDecrease(id)} >-</button>
+          <button className='btn__inc' onClick={()=> handleIncrease(id)} >+</button>
 
         </div>
         
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,22 +2,70 @@ import React, { Component } from 'react'
 import Counter from '../../Components/Counter'
 import './index.css'
 
-
+const INITIAL_COUNTERS_QIM = {
+  1: {
+    value: 0,
+    amount: 1,
+  },
+  2: {
+    value: 0,
+    amount: 1,
+  },
+  3: {
+    value: 0,
+    amount: 1,
+  },
+}
 
 export default class Home extends Component {
 
+  state = { counters: INITIAL_COUNTERS_QIM,}
+
+  handleIncrease = (id) => {
+    this.setState((prevState) => ({
+      counters: {
+        ...prevState.counters,
+        
+        [id]: {
+          ...prevState.counters[id],
+          value: prevState.counters[id].value + prevState.counters[id].amount,
+        },
+      },
+    }));
+  };
+
+  handleDecrease = (id) => {
+    if (this.state.counters[id].value !== 0) {
+      this.setState((prevState) => ({
+        counters: {
+          ...prevState.counters,
+          [id]: {
+            ...prevState.counters[id],
+            value: prevState.counters[id].value - prevState.counters[id].amount,
+          },
+        },
+      }));
+    }
+  };
 
 
-  
   render() {
+    console.log(Object.entries(this.state.counters));
     return (
       <div className="home_contain">
-        
-      <Counter />
-      <Counter />
-      <Counter />
+      {Object.entries(this.state.counters).map(([key,object]) =>
+          <Counter 
+            key={key}
+            id= {key}
+            value ={object.value}
+            handleIncrease={this.handleIncrease}
+            handleDecrease={this.handleDecrease}
+          />
+      )} 
+      
 
       </div>
+      
     );
   }
 }
