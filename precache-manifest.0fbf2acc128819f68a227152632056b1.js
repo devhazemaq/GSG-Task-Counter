38f03f40c2e44d645ac5f1c049922f7c5056b836
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-12 13:32:07 +0300

    Updates

---
+++ precache-manifest.0fbf2acc128819f68a227152632056b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a96022e5297a3415c61",
+    "url": "/static/css/main.0b3d67be.chunk.css"
+  },
+  {
+    "revision": "4a96022e5297a3415c61",
+    "url": "/static/js/main.4a96022e.chunk.js"
+  },
+  {
+    "revision": "cb8b415617bee276cc56",
+    "url": "/static/js/1.cb8b4156.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "391a9ad3dea970725fdb79b71044e255",
+    "url": "/index.html"
+  }
+];
