f7367297455ae4b4e5d4049dc145237de4033a15
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-12 10:00:08 +0300

    Modify the design and make it more coordinated than before

--- src/Components/Counter/index.jsx
+++ src/Components/Counter/index.jsx
@@ -1,44 +1,23 @@
-import React, { Component } from 'react';
-import './index.css';
+import React, { Component } from 'react'
+
+import "./index.css"
 
 export default class Counter extends Component {
-  state = {
-    counter: 0,
-  };
-
-  handleIncrease = () => {
-    this.setState(
-      (prevState) => ({
-        counter: prevState.counter + 1,
-      }),
-      () => {
-        this.props.onChange(1);
-      }
-    );
-  };
-
-  handleDecrease = () => {
-    this.setState(
-      (prevState) => ({
-        counter: prevState.counter - 1,
-      }),
-      () => {
-        this.props.onChange(-1);
-      }
-    );
-  };
-
   render() {
+
+
     return (
-      <div>
-        <button className="btn__counter btn__increment" onClick={this.handleIncrease}>
-          +
-        </button>
-        <span className="btn__counter span">{this.state.counter}</span>
-        <button className="btn__counter btn__decrement" onClick={this.handleDecrease}>
-          -
-        </button>
+      <div className='main__counter'> 
+
+        <div className='counter__ele'>
+          <span className='span__show'>0</span>
+          <button className='btn__dec' >-</button>
+          <button className='btn__inc'>+</button>
+
+        </div>
+        
+        
       </div>
-    );
+    )
   }
 }
